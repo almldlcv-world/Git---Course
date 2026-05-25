e3d515a795f2cb7688726ae0e3ad9a96a65d1933
Totan <totanbar42@gmail.com>
2026-05-26 00:57:09 +0530

    champ

--- Champ.java
+++ Champ.java
@@ -4,6 +4,12 @@ public class Champ{
         System.out.println("I'm Roamn Regins!!!");
         System.out.println("Branch -> Dev");
         System.out.println("Hello Universe!!");
+        name("The ROCK!!");
+    }
 
+
+    public static String name(String name) {
+        System.out.println("The Final BOSS");
+        return name;
     }
 }
